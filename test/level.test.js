f0666c8d5db0e0dc7a6b70a87c057ed8bae31e0c
Richard Rodger <richard@ricebridge.com>
2013-03-17 20:52:15 +0000

    tests pass

---
+++ test/level.test.js
@@ -0,0 +1,49 @@
+/* Copyright (c) 2013 Richard Rodger, MIT License */
+"use strict";
+
+
+var assert = require('assert')
+
+
+var seneca = require('seneca')
+
+
+
+var shared = seneca.test.store.shared
+
+
+
+var si = seneca()
+si.use(require('..'),{
+  folder:'db'
+})
+
+si.__testcount = 0
+var testcount = 0
+
+
+describe('level', function(){
+  it('basic', function(done){
+    testcount++
+    shared.basictest(si,done)
+  })
+
+
+  it('extra', function(done){
+    testcount++
+    extratest(si,done)
+  })
+
+
+  it('close', function(done){
+    shared.closetest(si,testcount,done)
+  })
+})
+
+
+
+function extratest(si,done) {
+  console.log('EXTRA')
+  si.__testcount++
+  done()
+}
