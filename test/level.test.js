d3f565ba348ef395bdcb15c8a9221190747e1002
marcopiraccini <marco.piraccini@gmail.com>
2015-09-18 09:56:34 +0200

    Moved to seneca-store-test 1.0.0
     Please enter the commit message for your changes. Lines starting

--- test/level.test.js
+++ test/level.test.js
@@ -1,12 +1,17 @@
 /* Copyright (c) 2013 Richard Rodger, MIT License */
 
+/*jslint node: true */
+
 "use strict";
 
-var Seneca = require('seneca');
-var Lab = require('lab');
-var CommonTests = require('seneca-store-test');
+var _ = require('lodash');
+var seneca = require('seneca');
+var shared = require('seneca-store-test');
+var fs = require('fs');
 
+var Lab = require('lab');
 var lab = exports.lab = Lab.script();
+
 var describe = lab.describe;
 var it = lab.it;
 
@@ -14,22 +19,32 @@ var tmpdir = require('osenv').tmpdir;
 var dir = tmpdir() + '/test-seneca-level-store';
 require('mkdirp').sync(dir);
 
-var seneca = Seneca({ log: 'silent' });
-seneca.use(require('..'), {
-  folder: dir
-})
 
-var testcount = 0;
-seneca.__testcount = 0;
+var incrementConfig = _.assign({
+  map: { '-/-/incremental': '*' },
+  auto_increment: true
+});
 
-describe('leveldb Store', function () {
+var si = seneca();
+si.use(require('..'), {folder: dir });
+si.use(require('..'), incrementConfig);
 
-  it('Common Tests', function (done) {
-    testcount++;
-    CommonTests.basictest(seneca, done);
-  })
+describe('Level Test', function () {
 
-  it('Common Tests Completed', function (done) {
-    CommonTests.closetest(seneca, testcount, done);
-  })
-})
+   shared.basictest({
+    seneca: si,
+    script: lab
+  });
+
+  // Not sure these should be supported by Level
+  // shared.sorttest({
+  //   seneca: si,
+  //   script: lab
+  // });
+  //
+  // shared.limitstest({
+  //   seneca: si,
+  //   script: lab
+  // });
+
+});
