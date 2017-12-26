f794a571e79ad397bfc0449560ec5862ff74cc07
EamonnK <amen_cionna@hotmail.com>
2017-12-26 15:37:31 +0000

    Code quality control updated - .eslintrc.js added/prettier installed

--- level-store.js
+++ level-store.js
@@ -15,103 +15,125 @@ var internals = {
   name: 'level-store'
 }
 
-module.exports = function (opts) {
+module.exports = function(opts) {
   var seneca = this
   var desc
   var dbmap = {}
 
-  opts = seneca.util.deepextend({
-    folder: '.',
-    valueEncoding: 'json',
-    sync: false,
-    fillCache: true
-  }, opts)
+  opts = seneca.util.deepextend(
+    {
+      folder: '.',
+      valueEncoding: 'json',
+      sync: false,
+      fillCache: true
+    },
+    opts
+  )
 
   /**
    * Get the levelup reference
    */
-  var get_db = function get_db (ent, done) {
+  var get_db = function get_db(ent, done) {
     var folder = internals.makefolderpath(opts.folder, ent)
     var db = dbmap[folder]
     if (db) {
       return done(null, db)
     }
-    internals.ensurefolder(folder, internals.error(done, function () {
-      db = dbmap[folder]
-      if (db) {
-        return done(null, db)
-      }
-      try {
-        db = dbmap[folder] = LevelQuery(Levelup(folder, opts))
-        db.query.use(JsonqueryEngine())
-      }
-      catch (e) {
+    internals.ensurefolder(
+      folder,
+      internals.error(done, function() {
         db = dbmap[folder]
         if (db) {
           return done(null, db)
         }
-        return done(e)
-      }
-      return done(null, db)
-    }))
+        try {
+          db = dbmap[folder] = LevelQuery(Levelup(folder, opts))
+          db.query.use(JsonqueryEngine())
+        } catch (e) {
+          db = dbmap[folder]
+          if (db) {
+            return done(null, db)
+          }
+          return done(e)
+        }
+        return done(null, db)
+      })
+    )
   }
 
   var store = {
     name: internals.name,
 
-    close: function (args, done) {
+    close: function(args, done) {
       // Closes all the dbs
-      seneca.util.recurse(_.values(dbmap), function (db, next) {
-        db.close(next)
-      }, done)
+      seneca.util.recurse(
+        _.values(dbmap),
+        function(db, next) {
+          db.close(next)
+        },
+        done
+      )
     },
 
-    save: function (args, done) {
+    save: function(args, done) {
       var ent = args.ent
       var update = !!ent.id
 
       if (!update) {
-        ent.id = (ent.id$ !== void 0) ? ent.id$ : Uuid()
-        delete (ent.id$)
+        ent.id = ent.id$ !== void 0 ? ent.id$ : Uuid()
+        delete ent.id$
       }
 
-      get_db(ent, internals.error(done, function (db) {
-        db.put(ent.id, ent.data$(false), function (err) {
-          if (err) {
-            return done(err)
-          }
+      get_db(
+        ent,
+        internals.error(done, function(db) {
+          db.put(ent.id, ent.data$(false), function(err) {
+            if (err) {
+              return done(err)
+            }
 
-          seneca.log.debug('save/' + (update ? 'update' : 'insert'), ent, desc)
-          done(null, ent)
+            seneca.log.debug(
+              'save/' + (update ? 'update' : 'insert'),
+              ent,
+              desc
+            )
+            done(null, ent)
+          })
         })
-      }))
+      )
     },
 
-    load: function (args, done) {
+    load: function(args, done) {
       var qent = args.qent
       var q = args.q
       var qq = internals.fixquery(qent, q)
 
       if (!qq.id) {
-        return store.list(args, internals.error(done, function (list) {
-          done(null, list[0])
-        }))
+        return store.list(
+          args,
+          internals.error(done, function(list) {
+            done(null, list[0])
+          })
+        )
       }
 
-      get_db(qent, internals.error(done, function (db) {
-        db.get(qq.id, function (err, data) {
-          if (err) {
-            return err.notFound ? done(null, null) : done(err)
-          }
+      get_db(
+        qent,
+        internals.error(done, function(db) {
+          db.get(qq.id, function(err, data) {
+            if (err) {
+              return err.notFound ? done(null, null) : done(err)
+            }
 
-          var fent = qent.make$(data)
-          seneca.log.debug('load', q, fent, desc)
-          done(null, fent)
+            var fent = qent.make$(data)
+            seneca.log.debug('load', q, fent, desc)
+            done(null, fent)
+          })
         })
-      }))
+      )
     },
 
-    remove: function (args, done) {
+    remove: function(args, done) {
       var qent = args.qent
       var q = args.q
 
@@ -120,135 +142,154 @@ module.exports = function (opts) {
       var load = _.isUndefined(q.load$) ? true : q.load$ // default true
       var qq = internals.fixquery(qent, q)
 
-      get_db(qent, internals.error(done, function (db) {
-        if (!_.isUndefined(qq.id) && load && !all) {
-          return db.get(qq.id, { fillCache: opts.fillCache }, function (err, data) {
-            if (err) {
-              return err.notFound ? done(null, null) : done(err)
-            }
+      get_db(
+        qent,
+        internals.error(done, function(db) {
+          if (!_.isUndefined(qq.id) && load && !all) {
+            return db.get(qq.id, { fillCache: opts.fillCache }, function(
+              err,
+              data
+            ) {
+              if (err) {
+                return err.notFound ? done(null, null) : done(err)
+              }
 
-            var fent = qent.make$(data)
-            seneca.log.debug('remove', q, fent, desc)
-            do_remove(db, fent)
-          })
-        }
+              var fent = qent.make$(data)
+              seneca.log.debug('remove', q, fent, desc)
+              do_remove(db, fent)
+            })
+          }
 
-        do_remove(db)
-      }))
+          do_remove(db)
+        })
+      )
 
-      function do_remove (db, maybefent) {
+      function do_remove(db, maybefent) {
         if (!_.isUndefined(qq.id)) {
-          return db.del(qq.id, {sync: opts.sync}, function (err) {
+          return db.del(qq.id, { sync: opts.sync }, function(err) {
             if (err) {
               return done(err)
             }
             seneca.log.debug('remove/one', q, maybefent, desc)
             if (load) {
               done(null, maybefent)
-            }
-            else {
+            } else {
               done()
             }
           })
         }
         if (all) {
-          store.list(args, internals.error(done, function (list) {
-            seneca.util.recurse(
-              list,
-              function (fent, next) {
-                db.del(fent.id, { sync: opts.sync }, next)
-              },
-              function (err) {
+          store.list(
+            args,
+            internals.error(done, function(list) {
+              seneca.util.recurse(
+                list,
+                function(fent, next) {
+                  db.del(fent.id, { sync: opts.sync }, next)
+                },
+                function(err) {
+                  if (err) {
+                    return done(err)
+                  }
+
+                  seneca.log.debug('remove/all', q, desc)
+
+                  if (load) {
+                    done(null, list[0] || undefined)
+                  } else {
+                    done()
+                  }
+                }
+              )
+            })
+          )
+        } else {
+          store.load(
+            args,
+            internals.error(done, function(item) {
+              if (!item) {
+                return done()
+              }
+              db.del(item.id, { sync: opts.sync }, function(err) {
                 if (err) {
                   return done(err)
                 }
-
-                seneca.log.debug('remove/all', q, desc)
-
+                seneca.log.debug('remove/one', q, item, desc)
                 if (load) {
-                  done(null, list[0] || undefined)
-                }
-                else {
+                  done(null, item)
+                } else {
                   done()
                 }
-              }
-            )
-          }))
-        }
-        else {
-          store.load(args, internals.error(done, function (item) {
-            if (!item) {
-              return done()
-            }
-            db.del(item.id, { sync: opts.sync }, function (err) {
-              if (err) {
-                return done(err)
-              }
-              seneca.log.debug('remove/one', q, item, desc)
-              if (load) {
-                done(null, item)
-              }
-              else {
-                done()
-              }
+              })
             })
-          }))
+          )
         }
       }
     },
 
-    list: function (args, done) {
+    list: function(args, done) {
       var qent = args.qent
       var q = args.q
 
-      get_db(qent, internals.error(done, function (db) {
-        // var mq = metaquery(qent, q)
-        var n = 1
-        var list = []
+      get_db(
+        qent,
+        internals.error(done, function(db) {
+          // var mq = metaquery(qent, q)
+          var n = 1
+          var list = []
 
-        // saves the sorting / limit / skip / fields params
-        var additionaParams = {
-          sort$: q.sort$,
-          limit$: q.limit$,
-          skip$: q.skip$,
-          fields$: q.fields$,
-          load$: q.load$, // these are from remove action but in some cases (missing id) list is called
-          all$: q.all$ // these are from remove action but in some cases (missing id) list is called
-        }
-        // Remove the optional params from the query
-        q = _.omit(q, ['sort$', 'limit$', 'skip$', 'fields$', 'load$', 'all$'])
-        if (q.all$) { // useless
-          q = {}
-        }
+          // saves the sorting / limit / skip / fields params
+          var additionaParams = {
+            sort$: q.sort$,
+            limit$: q.limit$,
+            skip$: q.skip$,
+            fields$: q.fields$,
+            load$: q.load$, // these are from remove action but in some cases (missing id) list is called
+            all$: q.all$ // these are from remove action but in some cases (missing id) list is called
+          }
+          // Remove the optional params from the query
+          q = _.omit(q, [
+            'sort$',
+            'limit$',
+            'skip$',
+            'fields$',
+            'load$',
+            'all$'
+          ])
+          if (q.all$) {
+            // useless
+            q = {}
+          }
 
-        db.query(q)
-          .on('data', function (data) {
-            if (internals.filterdata(q, data)) {
-              list.push(qent.make$(data))
-            }
-          })
-          .on('error', function (err) {
-            if (--n) {
-              throw new Error('errored:' + n)
-            }
-            done(err)
-          })
-          .on('close', function () {
-          })
-          .on('end', function () {
-            if (--n) {
-              throw new Error('ended:' + n)
-            }
-            // Order / skip / fields mngmt
-            list = internals.fixlist(additionaParams, list)
-            seneca.log.debug('list', q, list.length, list[0], desc)
-            done(null, list)
-          })
-      }))
+          db
+            .query(q)
+            .on('data', function(data) {
+              if (internals.filterdata(q, data)) {
+                list.push(qent.make$(data))
+              }
+            })
+            .on('error', function(err) {
+              if (--n) {
+                throw new Error('errored:' + n)
+              }
+              done(err)
+            })
+            .on('close', function() {})
+            .on('end', function() {
+              if (--n) {
+                throw new Error('ended:' + n)
+              }
+              // Order / skip / fields mngmt
+              list = internals.fixlist(additionaParams, list)
+              seneca.log.debug('list', q, list.length, list[0], desc)
+              done(null, list)
+            })
+        })
+      )
     },
 
-    native: function (args, done) {
-      get_db(args.ent, function (err, db) {
+    native: function(args, done) {
+      get_db(args.ent, function(err, db) {
         done(err, db)
       })
     }
@@ -259,11 +300,11 @@ module.exports = function (opts) {
   return { name: store.name, tag: storedesc.tag }
 }
 
-internals.fixquery = function (qent, q) {
-  return q === null ? {} : _.isString(q) ? {id: q} : _.isString(q.id) ? q : q
+internals.fixquery = function(qent, q) {
+  return q === null ? {} : _.isString(q) ? { id: q } : _.isString(q.id) ? q : q
 }
 
-internals.filterdata = function (q, data) {
+internals.filterdata = function(q, data) {
   for (var p in q) {
     if (!~p.indexOf('$') && q[p] !== data[p]) {
       return null
@@ -278,7 +319,7 @@ internals.filterdata = function (q, data) {
 /**
  * sort$, skip$, limit$ and fields$ support.
  */
-internals.fixlist = function (q, list) {
+internals.fixlist = function(q, list) {
   // sort first
   if (q.sort$) {
     for (var sf in q.sort$) break
@@ -298,7 +339,7 @@ internals.fixlist = function (q, list) {
   }
 
   if (q.fields$) {
-    list = _.map(list, function (item) {
+    list = _.map(list, function(item) {
       return _.pick(item, q.fields$)
     })
   }
@@ -309,12 +350,12 @@ internals.fixlist = function (q, list) {
 /**
  * Return if the folder exists, create otherwise.
  */
-internals.ensurefolder = function (folder, cb) {
-  Fs.stat(folder, function (err, stat) {
+internals.ensurefolder = function(folder, cb) {
+  Fs.stat(folder, function(err, stat) {
     if (!err && stat.isDirectory()) {
       return cb()
     }
-    Fs.mkdir(folder, function (err) {
+    Fs.mkdir(folder, function(err) {
       if (err && err.code === 'EEXIST') {
         err = null
       }
@@ -323,8 +364,8 @@ internals.ensurefolder = function (folder, cb) {
   })
 }
 
-internals.makefolderpath = function (folder, ent) {
-  var canon = ent.canon$({object: true})
+internals.makefolderpath = function(folder, ent) {
+  var canon = ent.canon$({ object: true })
 
   var base = canon.base
   var name = canon.name
@@ -334,8 +375,8 @@ internals.makefolderpath = function (folder, ent) {
   return folderpath
 }
 
-internals.error = function (done, win) {
-  return function (err, out) {
+internals.error = function(done, win) {
+  return function(err, out) {
     if (err) {
       return done(err)
     }
